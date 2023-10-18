08e7599076a664ae690e394f35d2f544a02937d0
Sreeram Jagannath <sreeram.subbaray@tigeranalytics.com>
2023-10-19 00:09:58 +0530

    mvp

--- app.py
+++ app.py
@@ -4,53 +4,88 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 
+import openai
+
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
-print(OPENAI_API_KEY)
-# def translate(input_text, target_language, target_audience):
-#     # Use a translation library to translate the input text
-#     with st.spinner(text="Translating.."):
-#         time.sleep(1)
+def get_response(query, target_language, target_audience):
+    system_message = """
+    As an experienced and professional language translator, you specialize in translating Indian languages. 
+    Please gather the following information from the user:
 
-#     translated_text = "This is JUST a demo"
+    1. SOURCE TEXT: The text you want to translate.
+    2. TARGET LANGUAGE: The target language you want the text to be translated into.
+    3. TARGET AUDIENCE: The intended target audience. Specify if the audience is around 10-15 years old or working
+    professionals, or old people.
 
-#     # Return the translated text
-#     return translated_text
+    You will adjust the choice of words and language usage based on the specified target audience and return 
+    the translated text. Do not change any factual details from the source text.
+    """
 
-# if __name__ == "__main__":
+    final_prompt = f"""
+    SOURCE TEXT:
+    ```{query}```
+    TARGET LANGUAGE:
+    ```{target_language}```
+    TARGET AUDIENCE:
+    ```{target_audience}
 
-#     st.title("Localization Engine")
+    TRANSLATED TEXT:
+    """
 
-#     # Create a text input for the target language
-#     target_language = st.text_input("Target Language:")
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "system", "content": system_message},
+                {"role": "user", "content": final_prompt}
+    ])
+    
+    try:
+        translated_text = response.get("choices")[0].get("message").get("content")
+    except Exception as e:
+        st.error(body=e)
 
-#     # Create a text input for the target audience
-#     target_audience = st.text_input("Target Audience:")
-
-#     col1, col2 = st.columns(2)
-#     # Create a text input for the input text
-#     input_text = col1.text_area("Input Text:", height=300)
-
-#     # Create a text input for the output text
-#     output_text = col2.text_area("Output Text:", height=300, key="output_text")
-
-#     # Create a button to translate the text
-#     translate_button = st.button("Translate")
-
-#     if translate_button:
-#         # Get the input text
-#         input_text = input_text.strip()
-
-#         # Get the target language
-#         target_language = target_language.strip()
-
-#         # Translate the text
-#         translated_text = translate(input_text, target_language, target_audience)
-
-#         # Set the output text
-#         st.session_state["output_text"] = translated_text
+    return translated_text
 
 
-        
+
+
+def translate(inp_text, target_language, target_audience):
+    with st.spinner(text="Translating.."):
+        time.sleep(2)
+        translated_text = get_response(inp_text, target_language, target_audience)
+        # translated_text = "hello world"
+
+        st.session_state.output_text = translated_text
+
+
+if __name__ == "__main__":
+
+    st.title("Localization Engine")
+
+    col1, col2 = st.columns(2)
+    # List of the most common languages in India
+    common_languages = ["Hindi", "Bengali", "Telugu", "Marathi", "Tamil", "Urdu", "Gujarati", "Kannada", "Odia", "Punjabi"]
+
+    # Create a dropdown in Streamlit
+    target_language = col1.selectbox("Select Target Language", common_languages, index=0)
+
+    # Create a text input for the target audience
+    target_audience = col2.text_input(
+        "Target Audience:", 
+        value="young children",
+        placeholder="young, college going students | working professionals | old people")
+
+    st.divider()
+
+    col1, col2 = st.columns(2)
+    # Create a text input for the input text
+    sample_input_text = "Once upon a time, there lived a farmer in a village."
+    input_text = col1.text_area("Input Text:", height=300, value=sample_input_text )
+
+    # Create a text input for the output text
+    output_text = col2.text_area("Output Text:", height=300, key="output_text")
+
+    # Create a button to translate the text
+    translate_button = st.button("Translate", on_click=translate, args=(input_text, target_language, target_audience,))
